ba129c3a9851a2d3762f90b65ef4ae50b0de93ed
Akmal Sidik <143138356+WP2-AkmalSidik@users.noreply.github.com>
2025-07-12 08:57:28 +0700

    refactor(user): memperbaiki nav

--- resources/views/layouts/components/guru/nav.blade.php
+++ resources/views/layouts/components/guru/nav.blade.php
@@ -2,12 +2,12 @@
         class="fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-800 border-t border-gray-100 dark:border-gray-700 py-2">
         <div class="flex justify-around max-w-2xl mx-auto">
             <a href="{{ route('guru.dashboard.index') }}"
-                class="flex flex-col items-center {{ request()->is('guru') ? 'text-bangala' : 'hover:text-bangala' }}">
+                class="flex flex-col items-center text-gray-400 {{ request()->is('guru') ? 'text-bangala' : 'hover:text-bangala' }}">
                 <i class="fas fa-home"></i>
                 <span class="text-xs mt-1">Beranda</span>
             </a>
             <a href="{{ route('guru.statistik') }}"
-                class="flex flex-col items-center {{ request()->is('guru/statistik*') ? 'text-bangala' : 'hover:text-bangala' }}">
+                class="flex flex-col items-center text-gray-400 {{ request()->is('guru/statistik*') ? 'text-bangala' : 'hover:text-bangala' }}">
                 <i class="fas fa-chart-line text-sm sm:text-base"></i>
                 <span class="text-xs mt-1">Statistik</span>
             </a>
